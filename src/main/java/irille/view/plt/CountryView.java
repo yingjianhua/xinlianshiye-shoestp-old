e994fa646d84b9b8d0330bf5ed582a43abda63b8
lijie <lijie@shoestp.cn>
2018-12-04 13:56:17 +0800

    更新

--- src/main/java/irille/view/plt/CountryView.java
+++ src/main/java/irille/view/plt/CountryView.java
@@ -4,9 +4,7 @@ import irille.pub.i18n.I18NFieldSerializer;
 import irille.view.BaseView;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import lombok.Data;
 
-@Data
 public class CountryView implements BaseView {
 
     private int id;
@@ -15,5 +13,45 @@ public class CountryView implements BaseView {
     private String shortName;
     private String flag;
     private boolean isDefault;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public boolean getIsDefault() {
+        return isDefault;
+    }
+
+    public void setIsDefault(boolean isDefault) {
+        this.isDefault = isDefault;
+    }
 
 }
