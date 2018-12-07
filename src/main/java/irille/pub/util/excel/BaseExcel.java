ad0b474235e76a50f1bfb6f8e434124c30247faa
lijie <lijie@shoestp.cn>
2018-12-07 16:45:30 +0800

    Excel 导出

--- src/main/java/irille/pub/util/excel/BaseExcel.java
+++ src/main/java/irille/pub/util/excel/BaseExcel.java
@@ -5,12 +5,20 @@ import irille.pub.util.excel.Entity.CellData;
 import irille.pub.util.excel.Entity.ExcelSettings;
 import irille.pub.util.excel.Other.DateCellStyle;
 import irille.pub.util.excel.Units.ExcelUnits;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.poi.POIXMLDocumentPart;
 import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.*;
 import org.openxmlformats.schemas.drawingml.x2006.spreadsheetDrawing.CTMarker;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.sql.Timestamp;
 import java.util.*;
@@ -158,6 +166,11 @@ public class BaseExcel {
         return result != null ? result : row.createCell(cell);
     }
 
+
+    public void setRowHeight(int y, short height) {
+        getRow(y).setHeightInPoints(height);
+    }
+
     /***
      * 读取指定列
      * @param x
@@ -201,6 +214,24 @@ public class BaseExcel {
             return workbook.getAllPictures().get(i);
         }
         return null;
+    }
+
+    /**
+     * @Description: 设置边框..默认黑色
+     * @date 2018/12/7 16:14
+     * @author lijie@shoestp.cn
+     */
+    public void setCellBorderStyle(Cell cell) {
+        CellStyle style = workbook.createCellStyle();
+        style.setBorderBottom(CellStyle.BORDER_THIN);
+        style.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+        style.setBorderLeft(CellStyle.BORDER_THIN);
+        style.setLeftBorderColor(IndexedColors.BLACK.getIndex());
+        style.setBorderRight(CellStyle.BORDER_THIN);
+        style.setRightBorderColor(IndexedColors.BLACK.getIndex());
+        style.setBorderTop(CellStyle.BORDER_THIN);
+        style.setTopBorderColor(IndexedColors.BLACK.getIndex());
+        cell.setCellStyle(style);
     }
 
 
@@ -301,6 +332,11 @@ public class BaseExcel {
     }
 
 
+    /**
+     * @Description: 获取Excel里的图片
+     * @date 2018/12/7 16:07
+     * @author lijie@shoestp.cn
+     */
     public Map<String, PictureData> getPic() {
         Map<String, PictureData> sheetIndexPicMap = new HashMap();
         if (workbook instanceof XSSFWorkbook) {
@@ -358,6 +394,11 @@ public class BaseExcel {
         saveTo(new File(filePath + fileName + suffix));
     }
 
+    /**
+     * @Description: 转换为内存字节流
+     * @date 2018/12/7 16:07
+     * @author lijie@shoestp.cn
+     */
     public ByteArrayOutputStream saveToOutputStream() throws IOException {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         workbook.write(outputStream);
@@ -373,5 +414,64 @@ public class BaseExcel {
             return;
         }
         throw new Exception("未指定保存文件路径及名称，请使用saveTO方法保存");
+    }
+
+
+    /**
+     * @Description: 添加图片到指定行列   前2位参数 右上角起始坐标   中间两位 右下角结束坐标
+     * @date 2018/12/7 16:06
+     * @author lijie@shoestp.cn
+     */
+    public void addPic(int i, int i1, int i2, int i3, String s) {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(String.valueOf(s));
+        httpGet
+                .setHeader(
+                        "User-Agent",
+                        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.79 Safari/537.1");
+        httpGet
+                .setHeader("Accept",
+                        "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+        BufferedImage bufferedImage = null;
+        try {
+            HttpResponse resp = httpclient.execute(httpGet);
+            if (HttpStatus.SC_OK == resp.getStatusLine().getStatusCode()) {
+                HttpEntity entity = resp.getEntity();
+                bufferedImage = ImageIO.read(entity.getContent());
+            }
+            String filesuffix = s.substring(s.lastIndexOf(".") + 1);
+            org.apache.commons.io.output.ByteArrayOutputStream byteArrayOut = new org.apache.commons.io.output.ByteArrayOutputStream();
+            ImageIO.write(bufferedImage, filesuffix, byteArrayOut);
+            Drawing patriarch = activeSheet.createDrawingPatriarch();
+            ClientAnchor anchor = null;
+            if (workbook instanceof XSSFWorkbook) {
+                anchor = new XSSFClientAnchor(0, 0, 255, 255, (short) i, i1, (short) i2, i3);
+            } else {
+                anchor = new HSSFClientAnchor(0, 0, 255, 255, (short) i, i1, (short) i2, i3);
+            }
+            setRowHeight(9, (short) 50);
+            anchor.setAnchorType(3);
+            int filetype = -1;
+            switch (filesuffix) {
+                case "jpg": {
+                    filetype = Workbook.PICTURE_TYPE_JPEG;
+                }
+                break;
+                case "png": {
+                    filetype = Workbook.PICTURE_TYPE_PNG;
+                }
+                break;
+                case "jpeg": {
+                    filetype = Workbook.PICTURE_TYPE_JPEG;
+                }
+                break;
+            }
+            if (filetype == -1) return;
+            patriarch.createPicture(anchor, workbook.addPicture(byteArrayOut.toByteArray(), filetype));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            httpclient.getConnectionManager().shutdown();
+        }
     }
 }
--- src/test/java/excelWrite.java
+++ src/test/java/excelWrite.java
@@ -1,4 +1,7 @@
 import irille.pub.util.excel.BaseExcel;
+import org.apache.poi.ss.usermodel.Cell;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -7,9 +10,12 @@ import irille.pub.util.excel.BaseExcel;
  * Time: 18:42
  */
 public class excelWrite {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BaseExcel baseExcel = new BaseExcel("S:\\鞋贸港下单表格20181205最新修改.xlsx");
-        baseExcel.addRow(baseExcel.getActiveSheets(), 9).createCell(1).setCellValue("你好");
+        Cell cell=baseExcel.addRow(baseExcel.getActiveSheets(), 9).createCell(2);
+        baseExcel.setCellBorderStyle(cell);
+        cell.setCellValue("你好");
+        baseExcel.addPic(1, 9, 2, 10, "https://image.shoestp.com/public/upload/cnt/ad/1d8c6e6c0ad9060d75e275b33c80d373.jpg");
         baseExcel.saveTo("S:\\13132");
     }
 }
