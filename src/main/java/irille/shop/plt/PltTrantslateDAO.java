9c1d0589af847539f1d415d38a9e100f99218b8e
wilson zhang <zhangwei@qqcom>
2019-01-15 19:31:32 +0800

    翻译缓存

--- src/main/java/irille/shop/plt/PltTrantslateDAO.java
+++ src/main/java/irille/shop/plt/PltTrantslateDAO.java
@@ -1,12 +1,20 @@
 package irille.shop.plt;
 
+import irille.platform.plt.View.Plttrantslate;
+import irille.platform.plt.View.TrantslateConditionView;
+import irille.pub.bean.Query;
+import irille.pub.bean.sql.SQL;
+import irille.pub.idu.IduDel;
 import irille.pub.idu.IduIns;
 import irille.pub.idu.IduOther;
 import irille.pub.svr.Env;
 import irille.pub.util.FormaterSql.FormaterSql;
 import irille.pub.util.TranslateLanguage.TranslateBean;
+import irille.view.Page;
 
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -39,4 +47,50 @@ public class PltTrantslateDAO {
         }
     }
 
+    public static class deltran extends IduDel<deltran, PltTrantslate> {
+        @Override
+        public void before() {
+        }
+    }
+    public  static Page ListTrantslate(List<TrantslateConditionView> tc,Integer start,Integer limit){
+        if (null == start ) {
+            start = 0;
+        }
+        if (null == limit||0==limit) {
+            limit = 15;
+        }
+        SQL sql=new SQL(){{
+           SELECT(PltTrantslate.class).FROM(PltTrantslate.class);
+           if(null!=tc){
+               for (TrantslateConditionView tv:tc){
+                   if(tv.getMode().equalsIgnoreCase(PltTrantslate.T.HASHCODE.toString())){
+                       WHERE(PltTrantslate.T.HASHCODE, "like '%"+tv.getContent()+"%'");
+                   }
+                   if(tv.getMode().equalsIgnoreCase(PltTrantslate.T.SOURCE_TEXT.toString())){
+                       WHERE(PltTrantslate.T.SOURCE_TEXT, "like '%"+tv.getContent()+"%'");
+                   }
+                   if(tv.getMode().equalsIgnoreCase(PltTrantslate.T.TARGET.toString())){
+                               WHERE(PltTrantslate.T.TARGET, "like '%"+tv.getContent()+"%'");
+                   }
+                   if(tv.getMode().equalsIgnoreCase(PltTrantslate.T.TARGET_TEXT.toString())){
+                       WHERE(PltTrantslate.T.TARGET_TEXT, "like '%"+tv.getContent()+"%'");
+                   }
+                   if(tv.getMode().equalsIgnoreCase(PltTrantslate.T.CREATED_TIME.toString())){
+                               WHERE(PltTrantslate.T.CREATED_TIME, tv.getCondition()+" ?", new Date(Long.parseLong(tv.getContent())));
+                   }
+               }
+           }
+        }};
+        System.out.println(sql);
+        Integer count= Query.sql(sql).queryCount();
+         List<Plttrantslate> list=Query.sql(sql.LIMIT(start,limit)).queryMaps().stream().map(bean ->new Plttrantslate(){{
+             setId((Integer) bean.get(PltTrantslate.T.PKEY.getFld().getCodeSqlField()));
+             setHashcode((String) bean.get(PltTrantslate.T.HASHCODE.getFld().getCodeSqlField()));
+             setSourceText((String) bean.get(PltTrantslate.T.SOURCE_TEXT.getFld().getCodeSqlField()));
+             setTarget((String) bean.get(PltTrantslate.T.TARGET.getFld().getCodeSqlField()));
+             setTargetText((String) bean.get(PltTrantslate.T.TARGET_TEXT.getFld().getCodeSqlField()));
+             setTime((Date) bean.get(PltTrantslate.T.CREATED_TIME.getFld().getCodeSqlField()));
+         }}).collect(Collectors.toList());
+        return  new Page(list,start,limit,count);
+    }
 }
