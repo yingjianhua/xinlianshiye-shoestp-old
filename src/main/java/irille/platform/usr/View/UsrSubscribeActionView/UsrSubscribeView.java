bd9e526e3b4e8b9603a50bf64454def91392cde1
郑建立 <2425228686@qq.com>
2019-01-21 15:31:53 +0800

    平台端部分接口

---
+++ src/main/java/irille/platform/usr/View/UsrSubscribeActionView/UsrSubscribeView.java
@@ -0,0 +1,10 @@
+package irille.platform.usr.View.UsrSubscribeActionView;
+
+import irille.view.BaseView;
+import lombok.Data;
+
+@Data
+public class UsrSubscribeView implements BaseView {
+    private Integer id;
+    private String email;
+}
--- src/main/java/irille/shop/usr/UsrSubscribeDAO.java
+++ src/main/java/irille/shop/usr/UsrSubscribeDAO.java
@@ -1,14 +1,78 @@
 package irille.shop.usr;
 
+import irille.platform.usr.View.UsrSubscribeActionView.UsrSubscribeView;
 import irille.pub.Log;
+import irille.pub.PropertyUtils;
+import irille.pub.bean.Query;
+import irille.pub.bean.sql.SQL;
+import irille.pub.idu.IduDel;
 import irille.pub.idu.IduIns;
-import irille.shop.usr.UsrPurchaseLineDAO.Ins;
+import irille.pub.idu.IduOther;
+import irille.view.Page;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UsrSubscribeDAO {
-	public static final Log LOG = new Log(UsrSubscribe.class);
-	public static class Ins extends IduIns<Ins, UsrPurchaseLine> {
-		public void before() {
-			super.before();
-		}
-	}
+    public static final Log LOG = new Log(UsrSubscribe.class);
+
+    public static class Ins extends IduIns<Ins, UsrPurchaseLine> {
+        public void before() {
+            super.before();
+        }
+    }
+
+    /**
+     * ———————————————————分割线(新平台)—————————————————————————
+     */
+    public static Page getSubscribeList(Integer start, Integer limit, String email) {
+        if(start == null){
+            start = 0;
+        }
+        if(limit == null){
+            limit = 10;
+        }
+        SQL sql = new SQL() {{
+            SELECT(UsrSubscribe.class).FROM(UsrSubscribe.class);
+            if (email != null) {
+                WHERE(UsrSubscribe.T.EMAIL, "like '%" + email + "%'");
+            }
+        }};
+        Integer count = Query.sql(sql).queryCount();
+        List<UsrSubscribeView> list = Query.sql(sql.LIMIT(start,limit)).queryMaps().stream().map(o -> new UsrSubscribeView(){{
+            setId((Integer)o.get(UsrSubscribe.T.PKEY.getFld().getCodeSqlField()));
+            setEmail((String)o.get(UsrSubscribe.T.EMAIL.getFld().getCodeSqlField()));
+        }}).collect(Collectors.toList());
+        return new Page(list,start,limit,count);
+    }
+    public static class deletes extends IduDel<deletes,UsrSubscribe>{
+        @Override
+        public void before() {
+            super.before();
+        }
+    }
+    public static void ins(String email){
+        UsrSubscribe usrSubscribe = new UsrSubscribe();
+        usrSubscribe.setEmail(email);
+        usrSubscribe.ins();
+    }
+    public static class upd extends IduOther<upd,UsrSubscribe>{
+        @Override
+        public void before() {
+        }
+
+        @Override
+        public void valid() {
+        }
+
+        @Override
+        public void run() {
+            UsrSubscribe dbBean = loadThisBeanAndLock();
+            PropertyUtils.copyProperties(dbBean, getB(), UsrSubscribe.T.EMAIL);
+            dbBean.upd();
+            super.run();
+        }
+    }
+
+    /**———————————————————分割线(新平台)END—————————————————————————*/
 }
