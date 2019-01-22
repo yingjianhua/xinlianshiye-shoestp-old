092c2b9cd0a48ff3d4a8ce0559a1f5875119157b
郑建立 <2425228686@qq.com>
2019-01-22 19:18:29 +0800

    订单管理(平台端)

---
+++ src/main/java/irille/platform/odr/view/OdrOrder/OdrOrderView.java
@@ -0,0 +1,35 @@
+package irille.platform.odr.view.OdrOrder;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import irille.view.BaseView;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+public class OdrOrderView implements BaseView {
+    private Long id;//pkey
+    private String purchase;//用户
+    private String orderNum;//订单号
+    @JsonFormat(pattern="yyyy/MM/dd HH:mm:ss", timezone="GMT+8")
+    private Date time;//下单时间
+    private Byte state;//订单状态
+    private Byte type;//订单类型
+    private String currency;//货币
+    private String expressNum;//运单号
+    private String delivery;//发货方式
+    private String pagRemarks;//包裹备注
+    private String odrRemarks;//订单备注
+    private BigDecimal freightPrice;//运费
+    private BigDecimal insurancePrice;//保险费
+    private BigDecimal prodPrice;//产品总价
+    private BigDecimal priceTotal;//总价
+    private String address;//地址
+    private String name;//名字
+    private String postalcode;//邮政编码
+    private String phone;//电话号码
+    private String paycontent;//支付内容
+    private Byte odrCancel;//取消原因
+    private String payType;//支付方式
+}
---
+++ src/main/java/irille/platform/odr/view/OdrOrder/StatusView.java
@@ -0,0 +1,10 @@
+package irille.platform.odr.view.OdrOrder;
+
+import irille.view.BaseView;
+import lombok.Data;
+
+@Data
+public class StatusView implements BaseView {
+    private Byte id;
+    private String value;
+}
