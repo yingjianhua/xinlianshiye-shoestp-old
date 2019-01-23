7ce7b5c5342f00eeb4d06cbdf473239f1e6ee47f
lingjian <251788868@qq.com>
2019-01-23 10:56:36 +0800

    （产品管理）产品属性

---
+++ src/main/java/irille/platform/pdt/View/PdtAttrView.java
@@ -0,0 +1,24 @@
+package irille.platform.pdt.View;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import irille.view.BaseView;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 产品属性对象类
+ * @author lingjian
+ * @date 2019/1/22 15:59
+ */
+@Data
+public class PdtAttrView implements BaseView {
+    private Integer id;
+//    @JsonSerialize(using = I18NFieldSerializer.class)
+    private String name; //名称
+//    @JsonSerialize(using = I18NFieldSerializer.class)
+    private Integer CATEGORY; //产品类目
+    private String createdBy;//建档员
+    @JsonFormat(pattern="yyyy/MM/dd HH:mm", timezone="GMT+8")
+    private Date createdTime;//建档时间
+}
