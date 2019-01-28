bbafeb3f325fb9197c25844dc924fc1a0f8a8a5e
lingjian <251788868@qq.com>
2019-01-28 10:26:54 +0800

    Merge branch '整合订购会' of D:\software\ideaWorkspaces\shoestp with conflicts.

--- src/main/java/irille/shop/pdt/PdtSizeDAO.java
+++ src/main/java/irille/shop/pdt/PdtSizeDAO.java
@@ -1,9 +1,11 @@
 package irille.shop.pdt;
 
+import java.io.Serializable;
+import java.util.List;
+import java.util.stream.Stream;
+
 import irille.core.sys.Sys;
 import irille.core.sys.Sys.OYn;
-import irille.core.sys.SysUser;
-import irille.platform.pdt.View.PdtSizeView;
 import irille.pub.Log;
 import irille.pub.PropertyUtils;
 import irille.pub.bean.BeanBase;
@@ -20,105 +22,10 @@ import irille.pub.validate.ValidForm;
 import irille.sellerAction.SellerAction;
 import irille.shop.pdt.PdtSize.T;
 import irille.shop.plt.PltConfigDAO;
-import irille.view.Page;
-
-import java.io.Serializable;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class PdtSizeDAO {
     public static final Log LOG = new Log(PdtSizeDAO.class);
-
-	/**
-	 * 查询产品尺寸
-	 * @author lingjian
-	 * @date 2019/1/22 13:37
- 	 * @param start
-	 * @param limit
-	 * @return
-	 */
-    public static Page listSize(String name, String productCategory,Integer start, Integer limit) {
-		if (null == start) {
-			start = 0;
-		}
-		if (null == limit) {
-			limit = 5;
-		}
-		System.out.println(name + "---"+ productCategory);
-		SQL sql = new SQL() {{
-			SELECT(PdtSize.class).FROM(PdtSize.class).WHERE(PdtSize.T.DELETED, "=0");
-			if(name != null){
-				WHERE(PdtSize.T.NAME, "like ?", "%" + name + "%");
-			}
-			if(productCategory != null){
-				WHERE(T.PRODUCT_CATEGORY, "=?", productCategory);
-			}
-		}};
-		Integer count= Query.sql(sql).queryCount();
-		List<PdtSizeView> list = Query.sql(sql.LIMIT(start, limit)).queryMaps().stream().map(bean -> new PdtSizeView() {{
-			setId((Integer) bean.get(T.PKEY.getFld().getCodeSqlField()));
-			setName((String) bean.get(T.NAME.getFld().getCodeSqlField()));
-
-			Integer s = (Integer) bean.get(T.PRODUCT_CATEGORY.getFld().getCodeSqlField());
-			if(null != s){
-				setProductCategory(BeanBase.load(PdtCat.class,s).getName());
-			}
-			setCreatedBy(BeanBase.load(SysUser.class, Integer.valueOf(String.valueOf(bean.get(T.CREATE_BY.getFld().getCodeSqlField())))).getLoginName());
-			setCreatedTime((Date) bean.get(T.CREATE_TIME.getFld().getCodeSqlField()));
-		}}).collect(Collectors.toList());
-		return new Page(list, start, limit, count);
-	}
-	/**
-	 * 新增产品尺寸
-	 * @author lingjian
-	 * @date 2019/1/22 13:37
-	 */
-	public static class InsSize extends IduIns<PdtSizeDAO.Ins, PdtSize> {
-		@Override
-		public void before() {
-			getB().setDeleted(OYn.NO.getLine().getKey());
-			getB().setCreateTime(Env.getTranBeginTime());
-			setB(translateUtil.autoTranslate(getB()));
-			super.before();
-		}
-	}
-
-	/**
-	 * 修改产品尺寸
-	 * @author lingjian
-	 * @date 2019/1/22 13:37
-	 */
-	public static class UpdSize extends IduUpd<PdtSizeDAO.Upd, PdtSize> {
-		@Override
-		public void before() {
-			PdtSize dbBean = loadThisBeanAndLock();
-			getB().setCreateTime(Env.getSystemTime());//自动生成修改时间
-			PropertyUtils.copyPropertiesWithout(
-					dbBean, translateUtil.autoTranslateByManageLanguage(getB(), true),
-					PdtSize.T.PKEY, PdtSize.T.SUPPLIER, PdtSize.T.CREATE_BY, PdtSize.T.CREATE_TIME, PdtSize.T.DELETED, PdtSize.T.ROW_VERSION);
-			setB(dbBean);
-		}
-	}
-
-	/**
-	 * 删除产品尺寸
-	 * @author lingjian
-	 * @date 2019/1/22 13:38
-	 */
-	public static class DelSize extends IduUpd<PdtSizeDAO.Del, PdtSize> {
-		@Override
-		public void before() {
-			super.before();
-			getB().setDeleted(OYn.YES.getLine().getKey());
-			PdtSize dbBean = loadThisBeanAndLock();
-			PropertyUtils.copyProperties(dbBean, getB(), PdtSize.T.DELETED);
-			setB(dbBean);
-		}
-	}
-
-
+    
     public static List<PdtSize> listSummary(Language lang,Integer type) {
     	
     	SQL sql = new I18NSQL(lang){{
