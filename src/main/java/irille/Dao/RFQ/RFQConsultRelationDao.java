7ebe89fb6d803b71e6c130092336a03821bdb621
yingjianhua <a86291151@163.com>
2019-01-31 15:41:53 +0800

    商家增加将询盘回收站的功能

--- src/main/java/irille/Dao/RFQ/RFQConsultRelationDao.java
+++ src/main/java/irille/Dao/RFQ/RFQConsultRelationDao.java
@@ -5,10 +5,15 @@ import java.util.List;
 import com.google.inject.ImplementedBy;
 
 import irille.Dao.RFQ.impl.RFQConsultRelationDaoImpl;
+import irille.Entity.RFQ.RFQConsultRelation;
 import irille.platform.rfq.view.RFQConsultRelationView;
 
 @ImplementedBy(RFQConsultRelationDaoImpl.class)
 public interface RFQConsultRelationDao {
 
 	List<RFQConsultRelationView> findAllViewByConsultId(Integer id);
+	
+	List<RFQConsultRelation> findAllByConsult_PkeySupplier_Pkey(String consultPkeys, Integer supplierPkey);
+	
+	void save(RFQConsultRelation bean);
 }
