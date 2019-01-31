d9a1406c04e24daa9919d69f87af52767d11a59d
yingjianhua <a86291151@163.com>
2019-01-31 13:48:04 +0800

    完善询盘审核功能

--- src/main/java/irille/Service/RFQ/Impl/RFQConsultServiceImpl.java
+++ src/main/java/irille/Service/RFQ/Impl/RFQConsultServiceImpl.java
@@ -50,9 +50,24 @@ public class RFQConsultServiceImpl implements RFQConsultService {
 			throw new WebMessageException(ReturnCode.valid_notnull, "请选择询盘");
 		}
 		RFQConsult consult = rFQConsultDao.findById(view.getPkey());
+		if(!consult.gtType().equals(RFQConsultType.RFQ)) {
+			//不是RFQ询盘不需要进行审核
+			throw new WebMessageException(ReturnCode.service_state_error, "类型错误");
+		}
+		//必须要询盘状态为待发布,审核状态为未审核
 		if(consult.gtStatus().equals(RFQConsultStatus.ready) && consult.gtVerifyStatus().equals(RFQConsultVerifyStatus.UNAUDITED)) {
-			//审核通过 ,必须要询盘状态为待发布,审核状态为未审核
-			consult.stVerifyStatus(verify?RFQConsultVerifyStatus.PASS:RFQConsultVerifyStatus.FAIL);
+			if(verify) {
+				//审核通过
+				consult.stVerifyStatus(RFQConsultVerifyStatus.PASS);
+				consult.stStatus(RFQConsultStatus.runing);
+			} else {
+				//审核不通过
+				consult.stVerifyStatus(RFQConsultVerifyStatus.FAIL);
+				//若询盘修改次数已经大于三次,则直接关闭询盘
+				if(consult.getChangeCount() > 3) {
+					consult.stStatus(RFQConsultStatus.close);
+				}
+			}
 		} else {
 			throw new WebMessageException(ReturnCode.service_state_error, "状态错误");
 		}
