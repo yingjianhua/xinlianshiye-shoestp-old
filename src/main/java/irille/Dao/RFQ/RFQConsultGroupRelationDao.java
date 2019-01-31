cf92e599a3adbb2eae2a9edc08014d2525f98396
yingjianhua <a86291151@163.com>
2019-01-31 14:06:11 +0800

    询盘分组操作实现

--- src/main/java/irille/Dao/RFQ/RFQConsultGroupRelationDao.java
+++ src/main/java/irille/Dao/RFQ/RFQConsultGroupRelationDao.java
@@ -1,8 +1,22 @@
 package irille.Dao.RFQ;
 
+import java.util.List;
+
+import com.google.inject.ImplementedBy;
+
+import irille.Dao.RFQ.impl.RFQConsultGroupRelationDaoImpl;
+import irille.Entity.RFQ.RFQConsultGroupRelation;
+
+@ImplementedBy(RFQConsultGroupRelationDaoImpl.class)
 public interface RFQConsultGroupRelationDao {
+	
+	void save(RFQConsultGroupRelation bean);
+	
+	RFQConsultGroupRelation findByConsult_PkeySupplier_Pkey(Integer consultPkey, Integer supplierPkey);
 	
 	Integer countByGroup_pkey(Integer groupId);
 	
 	void deleteByGroup_pkey(Integer groupId);
+	
+	List<RFQConsultGroupRelation> findAllByGroup_Pkey(Integer groupPkey);
 }
