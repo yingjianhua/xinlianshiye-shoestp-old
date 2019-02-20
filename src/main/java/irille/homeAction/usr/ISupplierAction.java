58e2eb3c6317b6d9ca7b91df0882f645cc39fe3c
yingjianhua <a86291151@163.com>
2019-02-20 21:04:31 +0800

    供应商信息

--- src/main/java/irille/homeAction/usr/ISupplierAction.java
+++ src/main/java/irille/homeAction/usr/ISupplierAction.java
@@ -2,12 +2,15 @@ package irille.homeAction.usr;
 
 import java.io.IOException;
 
+import org.json.JSONException;
+
 public interface ISupplierAction {
 
 	/**
 	 * 获取供应商详情
 	 * @throws IOException
 	 * @author Jianhua Ying
+	 * @throws JSONException 
 	 */
-	void getDetail() throws IOException;
+	void getDetail() throws IOException, JSONException;
 }
