a1810c2ecb08b3188cbda8c8cb26339299012b4a
yichao <yichao@shoestp.cn>
2019-02-22 11:50:41 +0800

    o2o 报名商品列表修复

--- src/main/java/irille/Service/Manage/O2O/Imp/O2OActicityServerImp.java
+++ src/main/java/irille/Service/Manage/O2O/Imp/O2OActicityServerImp.java
@@ -19,7 +19,6 @@ import irille.pub.Log;
 import irille.pub.exception.ReturnCode;
 import irille.pub.exception.WebMessageException;
 import irille.pub.idu.Idu;
-import irille.shop.pdt.Pdt;
 import irille.shop.pdt.PdtProduct;
 import irille.shop.usr.UsrSupplier;
 
@@ -41,6 +40,9 @@ public class O2OActicityServerImp implements IO2OActicityServer{
         O2O_Activity activityEntity = o2OActivityDao.getActivityInfoById(activity);
         if(null == activityEntity)
             throw LOG.err("noActivity","活动不存在");
+        if(activityEntity.getEndDate().before(new Date())){
+        	throw new WebMessageException(ReturnCode.failure, "活动已结束,无法报名参加");
+        }
         boolean flag = false;
         O2O_JoinInfo joinInfo = O2O_JoinInfo.chkUniqueActivity_supplier(false,activity,supplier.getPkey());
         if(null == joinInfo){
@@ -83,11 +85,11 @@ public class O2OActicityServerImp implements IO2OActicityServer{
                 }
             }else{
                 o2oPdt = new O2O_Product();
-                if(pdt.getProductType().equals(Pdt.OProductType.O2O.getLine().getKey())){
-                	o2oPdt.setVerifyStatus(O2O_ProductStatus.PASS.getLine().getKey());
-                }else{
+//                if(pdt.getProductType().equals(Pdt.OProductType.O2O.getLine().getKey())){
+//                	o2oPdt.setVerifyStatus(O2O_ProductStatus.PASS.getLine().getKey());
+//                }else{
                 	o2oPdt.setVerifyStatus(O2O_ProductStatus._DEFAULT.getLine().getKey());
-                }
+//                }
             }
             o2oPdt.setStatus(O2O_ProductStatus.ON.getLine().getKey());
             o2oPdt.setActivityId(activityEntity.getPkey());
