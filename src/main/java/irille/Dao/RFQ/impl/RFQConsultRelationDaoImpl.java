2057b2deb52778a02947d26bf248eb9afd7fd381
郑建立 <2425228686@qq.com>
2019-03-01 15:35:20 +0800

    修改删除报价(删除不掉) 原因：查询字段错误

--- src/main/java/irille/Dao/RFQ/impl/RFQConsultRelationDaoImpl.java
+++ src/main/java/irille/Dao/RFQ/impl/RFQConsultRelationDaoImpl.java
@@ -77,7 +77,7 @@ public class RFQConsultRelationDaoImpl implements RFQConsultRelationDao {
 	public List<RFQConsultRelation> findAllByConsult_PkeySupplier_Pkey(String consultPkeys, Integer supplierPkey) {
 		return Query.SELECT(RFQConsultRelation.class)
 				.WHERE(RFQConsultRelation.T.SUPPLIER_ID, "=?", supplierPkey)
-				.WHERE(RFQConsultRelation.T.PKEY, "in (" + Stream.of(consultPkeys.split(",")).map(i -> "?").collect(Collectors.joining(",")) + ")", Stream.of(consultPkeys.split(",")).map(Integer::new).toArray(Serializable[]::new))
+				.WHERE(RFQConsultRelation.T.CONSULT, "in (" + Stream.of(consultPkeys.split(",")).map(i -> "?").collect(Collectors.joining(",")) + ")", Stream.of(consultPkeys.split(",")).map(Integer::new).toArray(Serializable[]::new))
 				.queryList();
 	}
 
