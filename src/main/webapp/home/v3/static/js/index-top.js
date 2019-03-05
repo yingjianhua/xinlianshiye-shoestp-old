20c6e12158e1fbeeb838b448328c2076c3cd0466
xc <xiachao@shoestp.cn>
2019-03-05 16:57:46 +0800

    新版 广交会
    (cherry picked from commit ea0a919e8132c1733f7f755bfa4b807325b5dacb)
    (cherry picked from commit 0169561941404b0dec82e32b533f45afe9c48686)

--- src/main/webapp/home/v3/static/js/index-top.js
+++ src/main/webapp/home/v3/static/js/index-top.js
@@ -18,7 +18,11 @@ template: `<div>
                 <dt>
                     <img src="/home/v3/static/images/indextopren.png" alt="">
                     <div class="o2otoplikenum">
-                        <div style="height:  20px;">
+                        <div style="height:20px;">
+                         <div  v-if="!sysConfig.user">
+                            <a href="/home/usr_UsrPurchase_sign" style="border-right: 1px solid #b7b7b7;padding-right: 3px;color: #4fa2d7;font-size:12px;" target="_blank">Register</a>
+                            <a href="/home/usr_UsrPurchase_sign" style="color: #4fa2d7;font-size:12px;" target="_blank">Login</a>
+                         </div>
                         </div>
                         <p>My Shoestp</p>
                     </div>
@@ -57,7 +61,7 @@ template: `<div>
                         <li><a href="/home/usr_UsrMessages_center" target="_blank">RFQ List <i class="el-icon-arrow-right"></i></a></li>
                         <li><a href="/home/usr_UsrConsult_listView" target="_blank">My Inquiry <i class="el-icon-arrow-right"></i></a></li>
                         <li><a href="/home/usr_UsrPurchase_userIndex" target="_blank">My Account <i class="el-icon-arrow-right"></i></a></li>
-                        <li><a href="/home/usr_UsrSupplier_supplierEntry" target="_blank">我要开店 <i class="el-icon-arrow-right"></i></a></li>
+                        <li style="font-size: 15px;font-weight: bold;"><a href="/home/usr_UsrSupplier_supplierEntry" target="_blank">我要开店 <i class="el-icon-arrow-right"></i></a></li>
                     </ul>
                     <div class="after-login"><a href="/home/usr_UsrConsult_publishView" target="_blank">Submit RFQ</a></div>
                     <div class="hours  after-login">Get multiple quotes within 24 hours！</div>
@@ -70,7 +74,7 @@ template: `<div>
                         <li><a href="//home/usr_UsrMessages_center" target="_blank">RFQ List <i class="el-icon-arrow-right"></i></a></li>
                         <li><a href="/home/usr_UsrConsult_listView" target="_blank">My Inquiry <i class="el-icon-arrow-right"></i></a></li>
                         <li><a href="/home/usr_UsrPurchase_userIndex" target="_blank">My Account <i class="el-icon-arrow-right"></i></a></li>
-                        <li><a href="/home/usr_UsrSupplier_supplierEntry" target="_blank">我要开店 <i class="el-icon-arrow-right"></i></a></li>
+                        <li style="font-size: 15px;font-weight: bold;"><a href="/home/usr_UsrSupplier_supplierEntry" target="_blank">我要开店 <i class="el-icon-arrow-right"></i></a></li>
                     </ul>
                     <div class="after-login"><a href="/home/usr_UsrConsult_publishView" target="_blank">Submit RFQ</a></div>
                     <div class="hours after-login">Get multiple quotes within 24 hours！</div>
