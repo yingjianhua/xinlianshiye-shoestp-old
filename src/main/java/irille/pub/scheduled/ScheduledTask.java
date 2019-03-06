2a1b40826cdd3d7b4f7a8913de3046723e0367f6
yichao <yichao@shoestp.cn>
2019-03-06 10:21:53 +0800

    1

--- src/main/java/irille/pub/scheduled/ScheduledTask.java
+++ src/main/java/irille/pub/scheduled/ScheduledTask.java
@@ -10,47 +10,62 @@ import javax.servlet.ServletContextListener;
 import java.util.concurrent.LinkedBlockingDeque;
 
 @Slf4j
-public class ScheduledTask implements ServletContextListener{
-	
-	private LinkedBlockingDeque<Task> _tasks = new LinkedBlockingDeque<Task>();
-	
-	private Scheduler scheduler = null;
-	
-	public void initScheduledTask() {
+public class ScheduledTask implements ServletContextListener {
+    private static final int minute = 60 * 10;
+    private static final int hour = 60 * 10 * 60;
+
+    private LinkedBlockingDeque<Task> _tasks = new LinkedBlockingDeque<Task>();
+
+    private Scheduler scheduler = null;
+
+    public void initScheduledTask() {
 //		addTask(QuartzTest.class,1,"测试");
-		addTask(O2OActicityServerImp.class,1600,"O2O活动状态变更");
-	}
-	
-	public void addTask(Class clazz,int time,String name) {
-		Task t = new Task();
-		t.setClazz(clazz);
-		t.setTime(time);
-		t.setName(name);
-		_tasks.add(t);
-	}
-	
-	//启动计划任务
-	public void start() {
-        for(Task t:_tasks) {
-        	JobDetail job = JobBuilder.newJob(t.getClazz())
-        			.withIdentity(t.getClazz().getSimpleName() + "_Job", t.getClazz().getName().substring(0,t.getClazz().getName().lastIndexOf(".")))
-    	            .build();
-        	
-        	
-        	Trigger trigger = TriggerBuilder.newTrigger()
-    	            .withIdentity(t.getClazz().getSimpleName() + "_Trigger", t.getClazz().getName().substring(0,t.getClazz().getName().lastIndexOf(".")))
-    	            .startNow()
-    	            .withSchedule(SimpleScheduleBuilder.simpleSchedule()
-    	                .withIntervalInSeconds(t.getTime())
-    	                .repeatForever())
-    	            .build();
-        	
-        	 try {
-				scheduler.scheduleJob(job, trigger);
-			} catch (SchedulerException e) {
-				log.info(t.getName()+"任务出错");
-				continue;
-			}
+        addTask(O2OActicityServerImp.class, 5 * minute, "O2O活动状态变更");
+    }
+
+    public void addTask(Class clazz, int time, String name) {
+        Task t = new Task();
+        t.setClazz(clazz);
+        t.setTime(time);
+        t.setName(name);
+        _tasks.add(t);
+    }
+
+    //启动计划任务
+    public void start() {
+        for (Task t : _tasks) {
+            JobDetail job = JobBuilder.newJob(t.getClazz())
+                    .withIdentity(t.getClazz().getSimpleName() + "_Job", t.getClazz().getName().substring(0, t.getClazz().getName().lastIndexOf(".")))
+                    .build();
+
+
+            Trigger trigger = TriggerBuilder.newTrigger()
+                    .withIdentity(t.getClazz().getSimpleName() + "_Trigger", t.getClazz().getName().substring(0, t.getClazz().getName().lastIndexOf(".")))
+                    .startNow()
+                    .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                            .withIntervalInSeconds(t.getTime())
+                            .repeatForever())
+                    .build();
+
+            try {
+                scheduler.scheduleJob(job, trigger);
+            } catch (SchedulerException e) {
+                log.info(t.getName() + "任务出错");
+                continue;
+            }
+        }
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            initScheduledTask();
+            scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            System.out.println("<<<======启动计划任务======>>>");
+            start();
+        } catch (SchedulerException e) {
+            e.printStackTrace();
         }
     }
 
