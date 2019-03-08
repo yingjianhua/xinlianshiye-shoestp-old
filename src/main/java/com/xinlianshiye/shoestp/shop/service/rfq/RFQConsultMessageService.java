b11783b8af46c8538af88640fc3038b15abd3fa9
yingjianhua <a86291151@163.com>
2019-03-08 10:03:13 +0800

    询盘聊天消息列表接口 增加从指定消息开始查询 而不是从头开始查询的功能

--- src/main/java/com/xinlianshiye/shoestp/shop/service/rfq/RFQConsultMessageService.java
+++ src/main/java/com/xinlianshiye/shoestp/shop/service/rfq/RFQConsultMessageService.java
@@ -14,12 +14,13 @@ public interface RFQConsultMessageService {
 	 * 分页查询聊天信息
 	 * @param purchase 采购商
 	 * @param relationPkey 询盘关联主键
+	 * @param messageStartPkey 从指定message开始查询
 	 * @param start 开始记录数
 	 * @param limit 每页记录数
 	 * @author Jianhua Ying
 	 * @return 
 	 */
-	RFQConsultMessagesView page(UsrPurchase purchase, Integer relationPkey, Integer start, Integer limit);
+	RFQConsultMessagesView page(UsrPurchase purchase, Integer relationPkey, Integer messageStartPkey, Integer start, Integer limit);
 	
 	/**
 	 * 采购商发送聊天消息
