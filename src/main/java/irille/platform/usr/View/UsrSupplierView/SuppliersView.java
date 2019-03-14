d8383bebd67dc0cde74348229124d11ea6325f10
lingjian <251788868@qq.com>
2019-03-14 10:05:53 +0800

    商家入驻-整合svs

--- src/main/java/irille/platform/usr/View/UsrSupplierView/SuppliersView.java
+++ src/main/java/irille/platform/usr/View/UsrSupplierView/SuppliersView.java
@@ -10,12 +10,15 @@ import lombok.Data;
 @Data
 public class SuppliersView implements BaseView {
   private Integer id; // pkey
+  private Integer svsId; // svsid
 
   private String name; // 企业名称
   private String companyAddr; // 所在地
 
   @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
   private Date applicationTime; // 申请时间
+  @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+  private Date storeopenTime; // 店铺入驻时间
 
   private Byte storeStatus; // 店铺状态
   private Byte status; // 审核状态
