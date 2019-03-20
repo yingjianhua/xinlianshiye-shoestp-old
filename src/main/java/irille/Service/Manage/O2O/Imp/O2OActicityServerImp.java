77a9c40bb1c542c13956fa98fa27617346fedd5c
yichao <yichao@shoestp.cn>
2019-03-20 11:39:27 +0800

    解决冲突

--- src/main/java/irille/Service/Manage/O2O/Imp/O2OActicityServerImp.java
+++ src/main/java/irille/Service/Manage/O2O/Imp/O2OActicityServerImp.java
@@ -71,16 +71,9 @@ public class O2OActicityServerImp implements IO2OActicityServer, Job {
     if (null == tel || (null != tel) && "".equals(tel.trim())) {
       throw new WebMessageException(ReturnCode.failure, "请输入电话号码");
     }
-<<<<<<< HEAD
     if (!ValidRegex.regMarch(Regular.REGULAR_TEL, tel)) {
       throw new WebMessageException(ReturnCode.failure, "请输入格式正确的电话号码");
     }
-=======
-    if (!ValidRegex.regMarch(Regular.REGULAR_CHINATEL, tel)) {
-      throw new WebMessageException(ReturnCode.failure, "请输入格式正确的电话号码");
-    }
-
->>>>>>> refs/heads/整合订购会
     O2O_Activity activityEntity = o2OActivityDao.getActivityInfoById(activity);
     if (null == activityEntity) {
       throw LOG.err("noActivity", "活动不存在");
