d293c73269a2f31388bcfeb27a376717ab510311
lijie <lijie@shoestp.cn>
2019-03-21 19:02:28 +0800

    更新正则

--- src/main/java/irille/pub/validate/Regular.java
+++ src/main/java/irille/pub/validate/Regular.java
@@ -6,7 +6,9 @@ public class Regular {
   public static String REGULAR_TEL = "^[+\\d]?\\d{1,3}-\\d{1,16}$"; // 国外手机校验
   public static String REGULAR_EMAIL = "^[\\w]{1,32}@\\w{1,15}.\\w{2,5}$"; // 邮箱校验
   public static String REGULAR_PWD = "^[^\\s]{6,20}$"; // 密码校验(6-20位字母加数字)
-  public static String REGULAR_CHINATEL = "^1\\d{10}$"; // 国内手机校验
+  public static String REGULAR_CHINATEL =
+      "^((1\\d{10})|(\\d{4}-?\\d{8}))$"; // 国内手机/电话校验  格式 138111111111    0511-11111111
+  // 051111111111
   /** * 考虑到其他语种问题..首尾不能为符号 且 长度在1-32为之间 */
   public static String REGULAR_NAME =
       "^([^`~!@#$%^&*()+= -\\]\\[';/.,<>?:\"{}|]).{0,32}(?<![`~!@#$%^&*()+= -\\]\\[';/.,<>?:\"{}|])$"; // 校验名字
