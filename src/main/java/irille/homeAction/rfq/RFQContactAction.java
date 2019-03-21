e81a7e63e2394ac8334d8c7e0292a3d7025f31bf
yingjianhua <a86291151@163.com>
2019-03-21 16:00:18 +0800

    将前端表单校验提取到action中, 给分组名字增加长度校验 最大长度为10 且不能包含非文本字符

--- src/main/java/irille/homeAction/rfq/RFQContactAction.java
+++ src/main/java/irille/homeAction/rfq/RFQContactAction.java
@@ -1,13 +1,17 @@
 package irille.homeAction.rfq;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 import javax.inject.Inject;
 
+import com.xinlianshiye.shoestp.common.errcode.MessageBuild;
 import com.xinlianshiye.shoestp.shop.service.rfq.RFQPurchaseContactService;
 
 import irille.Filter.svr.ItpCheckPurchaseLogin.NeedLogin;
 import irille.homeAction.AbstractHomeAction;
+import irille.pub.exception.ReturnCode;
+import irille.pub.exception.WebMessageException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -49,6 +53,7 @@ public class RFQContactAction extends AbstractHomeAction implements IRFQContactA
   @Override
   @NeedLogin
   public void addGroup() throws IOException {
+    validAddGroup(groupName);
     rFQPurchaseContactService.addGroup(getPurchase(), groupName, curLanguage());
     write();
   }
@@ -79,5 +84,17 @@ public class RFQContactAction extends AbstractHomeAction implements IRFQContactA
   @NeedLogin
   public void page() throws IOException {
     write(rFQPurchaseContactService.page(getPurchase(), keyword, groupPkey, start, limit));
+  }
+
+  private static final Pattern groupNamePattern = Pattern.compile("^[^\\s]{1,10}$");
+
+  private void validAddGroup(String groupName) {
+    if (groupName == null
+        || groupName.trim().isEmpty()
+        || groupName.length() > 10
+        || !groupNamePattern.matcher(groupName).matches()) {
+      throw new WebMessageException(
+          MessageBuild.buildMessage(ReturnCode.please_input_group_name, curLanguage()));
+    }
   }
 }
