a0dcd7dc43976484076c0584aeda5d98228d0b8d
yingjianhua <a86291151@163.com>
2019-03-22 08:58:53 +0800

    由于Pattern是线程安全的, 所以直接初始化在静态变量里

--- src/main/java/com/xinlianshiye/shoestp/shop/service/usr/UsrPurchaseService.java
+++ src/main/java/com/xinlianshiye/shoestp/shop/service/usr/UsrPurchaseService.java
@@ -71,7 +71,7 @@ public interface UsrPurchaseService {
    */
   PurchaseView getAccount(UsrPurchase purchase);
 
-  public static final String password_regex = "^[^\\s]{6,20}$";
+  public static final Pattern password_regex = Pattern.compile("^[^\\s]{6,20}$");
   /**
    * 校验密码格式的有效性
    * <li>长度 目前至少8位
@@ -81,13 +81,13 @@ public interface UsrPurchaseService {
    * @author Jianhua Ying
    */
   default void validPassword(String password) {
-    if (password == null || !Pattern.compile(password_regex).matcher(password).matches()) {
+    if (password == null || !password_regex.matcher(password).matches()) {
       throw new WebMessageException(ReturnCode.valid_pwdRegex, "密码格式不正确");
     }
   }
 
-  public static final String mobile_regex = "^1\\d{10}$";
-  public static final String mobile_regex2 = "^[+\\d]?\\d{1,3}-\\d{6,16}$";
+  public static final Pattern mobile_regex = Pattern.compile("^1\\d{10}$");
+  public static final Pattern mobile_regex2 = Pattern.compile("^[+\\d]?\\d{1,3}-\\d{6,16}$");
   /**
    * 校验手机号码格式的有效性
    *
@@ -95,13 +95,13 @@ public interface UsrPurchaseService {
    */
   default void validMobile(String mobile) {
     if (mobile == null
-        || !Pattern.compile(mobile_regex).matcher(mobile).matches()
-        || !Pattern.compile(mobile_regex2).matcher(mobile).matches()) {
+        || !mobile_regex.matcher(mobile).matches()
+        || !mobile_regex2.matcher(mobile).matches()) {
       throw new WebMessageException(ReturnCode.valid_phoneRegex, "手机格式不正确");
     }
   }
 
-  public static final String email_regex = "^[\\w]{1,16}@+\\w{1,15}.\\w{2,5}$";
+  public static final Pattern email_regex = Pattern.compile("^[\\w]{1,16}@+\\w{1,15}.\\w{2,5}$");
 
   /**
    * 校验邮箱地址格式的有效性
@@ -109,7 +109,7 @@ public interface UsrPurchaseService {
    * @param email
    */
   default void validEmail(String email) {
-    if (email == null || !Pattern.compile(email_regex).matcher(email).matches()) {
+    if (email == null || !email_regex.matcher(email).matches()) {
       throw new WebMessageException(ReturnCode.valid_mailRegex, "邮箱格式不正确");
     }
   }
