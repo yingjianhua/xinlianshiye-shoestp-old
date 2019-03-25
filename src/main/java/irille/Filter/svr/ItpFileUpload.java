353745ce1cbe4da47dcb4394637375eb94f1cafd
lijie <lijie@shoestp.cn>
2019-03-25 18:17:27 +0800

    尝试性捕捉错误

--- src/main/java/irille/Filter/svr/ItpFileUpload.java
+++ src/main/java/irille/Filter/svr/ItpFileUpload.java
@@ -1,35 +1,38 @@
 package irille.Filter.svr;
 
-import org.apache.struts2.interceptor.FileUploadInterceptor;
-
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.config.ConfigurationException;
 
 import irille.pub.Exp;
 import irille.pub.Log;
 import irille.pub.exception.WebMessageException;
+import org.apache.struts2.interceptor.FileUploadInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ItpFileUpload extends FileUploadInterceptor {
-	private static final Log LOG = new Log(ItpFileUpload.class);
+  private static final Log LOG = new Log(ItpFileUpload.class);
+  private static final Logger logger = LoggerFactory.getLogger(ItpFileUpload.class);
 
-	public String intercept(ActionInvocation invocation) throws Exception {
+  public String intercept(ActionInvocation invocation) throws Exception {
 
-		String result = null;
-		try {
-			result = super.intercept(invocation);
-		} catch (Exception e) {
-			if (e instanceof Exp) {
-				e.printStackTrace();
-				throw e;
-			}
-			if (e instanceof ConfigurationException) {
-				throw LOG.err("upload", "上传出错，请检查文件类型与大小");
-			}
-			if (e instanceof WebMessageException) {
-				throw e;
-			}
-			throw LOG.err("unknow", e.getMessage());
-		}
-		return result;
-	}
+    String result = null;
+    try {
+      result = super.intercept(invocation);
+    } catch (Exception e) {
+      if (e instanceof Exp) {
+        e.printStackTrace();
+        throw e;
+      }
+      if (e instanceof ConfigurationException) {
+        throw LOG.err("upload", "上传出错，请检查文件类型与大小");
+      }
+      if (e instanceof WebMessageException) {
+        throw e;
+      }
+      e.printStackTrace();
+      throw LOG.err("unknow", e.getMessage());
+    }
+    return result;
+  }
 }
