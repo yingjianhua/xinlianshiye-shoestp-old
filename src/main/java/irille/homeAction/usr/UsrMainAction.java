f92e3a2f7e41bc1dc0668f66b35f94e2b807b037
chenlili <lili@shoestp.cn>
2019-03-27 20:12:29 +0800

    手机端注册接口，PC端注册手机唯一性

--- src/main/java/irille/homeAction/usr/UsrMainAction.java
+++ src/main/java/irille/homeAction/usr/UsrMainAction.java
@@ -109,6 +109,8 @@ public class UsrMainAction extends HomeAction<UsrMain> {
     if (Str.isEmpty(getPwd()) || Str.isEmpty(getPwdA()))
       throw new WebMessageException(
           MessageBuild.buildMessage(ReturnCode.valid_pwd_notnull, HomeAction.curLanguage()));
+    System.out.println("..."+uid);
+    System.out.println("..."+String.valueOf(CacheUtils.mailValid.getIfPresent(uid)));
     if (uid == null
         || CacheUtils.mailValid.getIfPresent(uid) == null
         || !String.valueOf(CacheUtils.mailValid.getIfPresent(uid))
@@ -154,9 +156,14 @@ public class UsrMainAction extends HomeAction<UsrMain> {
         getBean().setContacts(null);
       }
     } else {
-      if (!ValidRegex.regMarch(Regular.REGULAR_NAME, getBean().getNickname())) {
+      if(Str.isEmpty(getBean().getNickname())){
         throw new WebMessageException(
-            MessageBuild.buildMessage(ReturnCode.valid_nameRegex, HomeAction.curLanguage()));
+                MessageBuild.buildMessage(ReturnCode.valid_name_notnull, HomeAction.curLanguage()));
+      }else{
+        if (!ValidRegex.regMarch(Regular.REGULAR_NAME, getBean().getNickname())) {
+          throw new WebMessageException(
+                  MessageBuild.buildMessage(ReturnCode.valid_nameRegex, HomeAction.curLanguage()));
+        }
       }
     }
     if (getBean().getIdentity() == 0) {
@@ -165,6 +172,10 @@ public class UsrMainAction extends HomeAction<UsrMain> {
       if(Str.isEmpty(getTelPre())||Str.isEmpty(getTelAft())){
         getBean().setTelphone("");
       }else{
+        if(usrMainDao.validOnePhone(phone,getBean().getIdentity())){
+          throw new WebMessageException(
+                  MessageBuild.buildMessage(ReturnCode.service_phone_exists, HomeAction.curLanguage()));
+        }
         if (!ValidRegex.regMarch(Regular.REGULAR_TEL, phone)) {
           throw new WebMessageException(
                   MessageBuild.buildMessage(ReturnCode.valid_phoneRegex, HomeAction.curLanguage()));
@@ -175,6 +186,10 @@ public class UsrMainAction extends HomeAction<UsrMain> {
       if (Str.isEmpty(getBean().getTelphone())) {
         throw new WebMessageException(
             MessageBuild.buildMessage(ReturnCode.valid_phone_notnull, HomeAction.curLanguage()));
+      }
+      if(usrMainDao.validOnePhone(getBean().getTelphone(),getBean().getIdentity())){
+        throw new WebMessageException(
+                MessageBuild.buildMessage(ReturnCode.service_phone_exists, HomeAction.curLanguage()));
       }
       if (!ValidRegex.regMarch(Regular.REGULAR_CHINATEL, getBean().getTelphone())) {
         throw new WebMessageException(
@@ -199,57 +214,7 @@ public class UsrMainAction extends HomeAction<UsrMain> {
     write();
     CacheUtils.mailValid.invalidate(uid);
   }
-    /**
-     * 手机端注册(3.0)
-     *
-     * @author chen
-     */
-    public void phoneRegist() throws IOException {
-        String code = verifyCode();
-        logger.debug(String.format("Method:%s\r\nCode:%s,", "regist", code));
-        if (Str.isEmpty(code) || Str.isEmpty(getCheckCode()) || code.equals(getCheckCode()) == false) {
-            throw new WebMessageException(
-                    MessageBuild.buildMessage(
-                            ReturnCode.service_verification_code, HomeAction.curLanguage()));
-        }
-        if (Str.isEmpty(getPwd()) || Str.isEmpty(getPwdA()))
-            throw new WebMessageException(
-                    MessageBuild.buildMessage(ReturnCode.valid_pwd_notnull, HomeAction.curLanguage()));
 
-        insBefore();
-        if (getBean().getEmail() == null) {
-            throw new WebMessageException(
-                    MessageBuild.buildMessage(ReturnCode.valid_mail_notnull, HomeAction.curLanguage()));
-        }
-        // 忽略邮箱大小写
-        getBean().setEmail(getBean().getEmail().toLowerCase());
-        if (!ValidRegex.regMarch(Regular.REGULAR_EMAIL, getBean().getEmail())) {
-            throw new WebMessageException(
-                    MessageBuild.buildMessage(ReturnCode.valid_mailRegex, HomeAction.curLanguage()));
-        }
-        if (!ValidRegex.regMarch(Regular.REGULAR_PWD, pwd)) {
-            throw new WebMessageException(
-                    MessageBuild.buildMessage(ReturnCode.password_format, HomeAction.curLanguage()));
-        }
-        if (!pwd.equals(pwdA)) {
-            throw new WebMessageException(
-                    MessageBuild.buildMessage(ReturnCode.dif_password, HomeAction.curLanguage()));
-        }
-
-        UsrMain main = UsrMain.chkUniqueEmail(false, getBean().getEmail());
-        if (main != null) {
-            throw new WebMessageException(
-                    MessageBuild.buildMessage(ReturnCode.service_user_exists, HomeAction.curLanguage()));
-        }
-        getBean().setPassword(getPwd());
-        getBean().setIdentity((Byte.valueOf("0")));
-        getBean().setTelphone("");
-        ins.setB(getBean());
-        ins.commit();
-        insAfter();
-        write();
-
-    }
 
   /**
    * 发送邮件
@@ -283,6 +248,13 @@ public class UsrMainAction extends HomeAction<UsrMain> {
       if (main != null) {
         throw new WebMessageException(
             MessageBuild.buildMessage(ReturnCode.service_user_exists, HomeAction.curLanguage()));
+      }
+    }
+    if (type == 3) {
+      title = "User registration";
+      if (main != null) {
+        throw new WebMessageException(
+                MessageBuild.buildMessage(ReturnCode.service_user_exists, HomeAction.curLanguage()));
       }
     }
     if (type == 1) {
@@ -363,6 +335,34 @@ public class UsrMainAction extends HomeAction<UsrMain> {
             true);
         write();
         return;
+      case 3:
+        Cache cachea = CacheUtils.sendEm;
+        String valuea = String.valueOf(cachea.getIfPresent(getEmail()));
+        if (valuea != null && !valuea.equals("null")) {
+          Long time = Long.parseLong(valuea);
+          long now = new Date().getTime();
+          int interval = (int) ((now - time) / 1000);
+          if (interval < 60) {
+            throw new WebMessageException(
+                    MessageBuild.buildMessage(ReturnCode.send_ofen, HomeAction.curLanguage()));
+          }
+        }
+        SecureRandom secureRandoma = new SecureRandom();
+        Integer codea = secureRandoma.nextInt(999999);
+        CacheUtils.mailValid.put(codea.toString(), getEmail());
+        CacheUtils.sendEm.put(getEmail(), new Date().getTime());
+        mailer.sendMail(
+                EmailBuilder.startingBlank()
+                        .withSubject("Shoestp Rest Your Password")
+                        .prependTextHTML(
+                                mailTemplate
+                                        .get("forgetPassWord")
+                                        .replaceAll("\\{\\{CODE}}", String.valueOf(codea)))
+                        .from("Shoestp", "notice@service.shoestp.com")
+                        .to(getEmail())
+                        .buildEmail(),
+                true);
+        write();
     }
   }
 
