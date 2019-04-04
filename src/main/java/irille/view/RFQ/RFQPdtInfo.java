412aa751f78fcf6dbe6037c9280d8582a354d058
lijie <lijie@shoestp.cn>
2019-04-04 11:53:07 +0800

    产品询盘数据反填充-获取店铺名称修正

--- src/main/java/irille/view/RFQ/RFQPdtInfo.java
+++ src/main/java/irille/view/RFQ/RFQPdtInfo.java
@@ -1,5 +1,8 @@
 package irille.view.RFQ;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import irille.pub.i18n.I18NFieldSerializer;
 import irille.view.BaseView;
 import lombok.Data;
 
@@ -7,7 +10,10 @@ import lombok.Data;
 @Data
 public class RFQPdtInfo implements BaseView {
   private int id; // 产品的Id
+
+  @JsonSerialize(using = I18NFieldSerializer.class)
   private String supName;
+
   private String title;
   private String image;
   private String suplevel;
