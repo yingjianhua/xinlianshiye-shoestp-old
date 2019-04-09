67e1e05cfb65742b4812ad845a1335000cc43d7b
yichao <yichao@shoestp.cn>
2019-04-09 10:05:34 +0800

    点击量过滤中国/局域网

--- src/main/java/irille/temporary/action/PageHitsAction.java
+++ src/main/java/irille/temporary/action/PageHitsAction.java
@@ -3,34 +3,67 @@ package irille.temporary.action;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import irille.homeAction.HomeAction;
 import irille.pub.exception.ReturnCode;
 import irille.pub.exception.WebMessageException;
+import irille.pub.util.ipUtils.City;
 import irille.temporary.dao.PageHitsDao;
 import irille.temporary.entity.PageHits;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 /** @author liyichao */
+@Slf4j
 public class PageHitsAction extends HomeAction<PageHits> {
 
   @Getter @Setter private String pageName;
   @Getter @Setter private String element;
 
+  private static final String LOCAL = "0:0:0:0:0:0:0:1";
+
+  private static final String LAN = "局域网";
+
+  private static final String CHINA = "中国";
+
   private PageHitsDao hitsDao = new PageHitsDao();
 
   public void click() throws IOException {
-    if (null == getPageName() || (null != getPageName() && "".equals(getPageName().trim()))) {
+    if (isBlank(getPageName())) {
       throw new WebMessageException(ReturnCode.failure, "非法参数");
     }
-    if (null == getElement() || (null != getElement() && "".equals(getElement().trim()))) {
+    if (isBlank(getElement())) {
       throw new WebMessageException(ReturnCode.failure, "非法参数");
     }
-    hitsDao.click(getPageName(), getElement());
-    write();
+    HttpServletRequest request = ServletActionContext.getRequest();
+    String ip = request.getRemoteAddr();
+    if (LOCAL.equals(ip)) {
+      write();
+    } else {
+      String[] areas = City.find(ip);
+      if (null != areas && areas.length > 0) {
+        String country = areas[0];
+        log.info(country + "来访,ip地址为" + ip);
+        if (!LAN.equals(country) && !CHINA.equals(country)) {
+          hitsDao.click(getPageName(), getElement());
+          write();
+        } else {
+          write();
+        }
+      } else {
+        write();
+      }
+    }
+  }
+
+  private boolean isBlank(String str) {
+    return null == str || (null != str && "".equals(str.trim()));
   }
 
   public void statistics() throws Exception {
@@ -46,5 +79,9 @@ public class PageHitsAction extends HomeAction<PageHits> {
     json.put("元素时间点击量", elementTimeHits);
     json.put("页面时间点击量", pageTimeHits);
     writerOrExport(json);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(City.find("192.168.100.46")[0]);
   }
 }
